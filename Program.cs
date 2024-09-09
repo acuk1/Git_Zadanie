37d13a5c08cab842c13bb061f7ab7c0cacefaaad
ZariponkoAndRylovenko <dinasamsitdinova@gmail.com>
2024-09-09 11:22:34 +0500

    Third program

--- Program.cs
+++ Program.cs
@@ -11,16 +11,20 @@ namespace Metod
         /*
          * ЯХА
          * ЯХА
-         */
         static int Max(int a, int b, int c)
         {
             return Math.Max(Math.Max(a, b), c);
         }
-       
+       */
+        static double Ctg(double a)
+        {
+          return (Math.Cos(a) / Math.Sin(a));
+        }
         static void Main(string[] args)
         {
-            Max(3, 10, 8);
-            Console.WriteLine($"макс число: " + Max(3,5,8));
+            /*Max(3, 10, 8);
+            Console.WriteLine($"макс число: " + Max(3,5,8));*/
+            Console.WriteLine($"Это реально круто: " + Ctg(10));
         }
     }
 }
