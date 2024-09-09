24169b415596230c24a3ef0379c5573a201ca151
Dina Masha <dinasamsitdinova@gmail.com>
2024-09-09 11:06:37 +0500

    ETO REALNO KRUTO

--- Program.cs
+++ Program.cs
@@ -8,8 +8,19 @@ namespace Metod
 {
     class Program
     {
+        /*
+         * ЯХА
+         * ЯХА
+         */
+        static int Max(int a, int b, int c)
+        {
+            return Math.Max(Math.Max(a, b), c);
+        }
+       
         static void Main(string[] args)
         {
+            Max(3, 10, 8);
+            Console.WriteLine($"макс число: " + Max(3,5,8));
         }
     }
 }
